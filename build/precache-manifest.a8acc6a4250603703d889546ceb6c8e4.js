e7605bf81444d3c85d11919335c111851abef457
megan-pg <megan.pardygokcu@unh.edu>
2020-08-14 21:18:42 -0400

    moved folders around

---
+++ build/precache-manifest.a8acc6a4250603703d889546ceb6c8e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53e144842820ea145ea34d20a7a46f43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d08a653edc6a369fc253",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e2cc871088868e8c04b8",
+    "url": "/static/js/2.4d16e705.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4d16e705.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d08a653edc6a369fc253",
+    "url": "/static/js/main.814c4b15.chunk.js"
+  },
+  {
+    "revision": "0934e5a0629121a12638",
+    "url": "/static/js/runtime-main.c542aba9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
